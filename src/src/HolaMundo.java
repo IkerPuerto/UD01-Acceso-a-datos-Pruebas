7dede2da4bdc06ef17fd6fa6ca530fd1eef42fea
iker <iker@IkerPuerto>
2024-09-24 18:21:10 +0200

    Commit 2

--- src/src/HolaMundo.java
+++ src/src/HolaMundo.java
@@ -1,10 +1,17 @@
 package src;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class HolaMundo {
 	public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Clase file");
+        File prueba = new File("//src/txt/prueba");
+        if (prueba.exists()) {
+        	System.out.println("Existe");
+        } else {
+        	System.out.println("No tienes ni idea");
+        }
     }
 }
 
